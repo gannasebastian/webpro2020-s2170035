c1170f4e43e6e912d6e0479d51f2cfb60c8263c1
gannasebastian <s21710035@student.ac.id>
2020-09-10 22:01:29 +0800

    exercise1

---
+++ pertemuan07/JSloop.js
@@ -0,0 +1,35 @@
+//Javascript loop
+
+//1. For
+/*for (let i = 1; i <= 10; i++) {
+    console.log(i); // menampilkan angka dari 1-10
+} 
+*/
+
+//2. For In\
+/*const buku = {
+    judul: "Harry Potter and The Philosopher's Stone",
+    pengarang: "J. K. Rowling",
+    tahun: 1997
+};
+ for (x in buku) {
+   console.log(x, ':', buku[x]);
+}*/
+
+//3. For Of
+/*const buku = ["Game of Thrones: A Song of Ice and Fire",  
+   "Harry Potter and The Philosopher's Stone", 
+   "Lord of The Rings: The Fellowship of The Ring"
+];
+for (x of buku) {
+  console.log(x);
+}*/
+
+//4. While
+let i = 1;
+
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+  
---
+++ pertemuan07/index.js
@@ -0,0 +1,59 @@
+//JavaScript Conditional
+//If else
+
+/*let nilaiAndi = 95;
+
+if (nilaiAndi > 80) {
+  console.log("SANGAT MEMUASKAN");
+} else if (nilaiAndi >= 60 && nilaiAndi <= 80) {
+  console.log("MEMUASKAN");
+} else {
+  console.log("JANGAN MENYERAH, COBA LAGI!");
+}
+*/
+
+//Switch Case
+/*let seragamSekolah;
+let hari = "senin";
+
+switch (hari) {
+  case "senin":
+    seragamSekolah = "kemeja putih bawahan merah";
+    break;
+  case "selasa":
+    seragamSekolah = "kemeja hijau bawahan hitam";
+    break;
+  case "rabu":
+    seragamSekolah = "kemeja putih bawahan putih";
+    break;
+  case "kamis":
+    seragamSekolah = "kemeja batik merah bawahan hitam";
+    break;
+  case "jumat":
+    seragamSekolah = "baju pramuka";
+    break;
+  default:
+    seragamSekolah = "baju bebas";
+}
+
+console.log(seragamSekolah); // Output: kemeja putih bawahan merah
+*/
+
+let seragamSekolah;
+let hari = "senin";
+
+switch (hari) {
+  case "senin":
+  case "selasa":
+  case "rabu":
+  case "kamis":
+    seragamSekolah = "kemeja putih bawahan merah";
+    break;
+  case "jumat":
+    seragamSekolah = "baju pramuka";
+    break;
+  default:
+    seragamSekolah = "baju bebas";
+}
+
+console.log(seragamSekolah); // Output: kemeja putih bawahan merah
