a90fe5f72493cc7967e6be2018ae4b34796ca49e
gannasebastian <s21710035@student.ac.id>
2020-09-22 14:31:57 +0800

    3rd Commit

--- pertemuan03/index.js
+++ pertemuan03/index.js
@@ -1,1 +1,7 @@
-console.log("Hello world, Welcome to Web Programming Class");
+//console.log("Hello world, Welcome to Web Programming Class");
+
+const firstName="Sebastian";
+let age=21;
+const isMarried = true;
+
+console.log("My name is "+ firstName);
