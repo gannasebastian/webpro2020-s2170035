ea85044b7876bb826efb3900e9c76db3ce2dcc95
gannasebastian <s21710035@student.ac.id>
2020-09-24 09:57:13 +0800

    3rd Commit

--- pertemuan03/index.js
+++ pertemuan03/index.js
@@ -1,7 +1,19 @@
 //console.log("Hello world, Welcome to Web Programming Class");
 
-const firstName="Sebastian";
+/*
+    1. Variable & Tipe Data
+*/
+
+const firstName="Sebastian"; 
 let age=21;
 const isMarried = true;
 
 console.log("My name is "+ firstName);
+console.log(age);
+console.log(isMarried);
+console.log("My name is "+ firstName +"I'm "+ age + " years old");
+
+// Validate the data type using typeof
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof isMarried);
