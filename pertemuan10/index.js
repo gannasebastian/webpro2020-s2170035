6b87172c61f8f17c692495d228c3ccfc16f6746a
gannasebastian <s21710035@student.ac.id>
2020-09-24 21:39:52 +0800

    10th Commit

--- pertemuan10/index.js
+++ pertemuan10/index.js
@@ -3,7 +3,55 @@
 //let nama="sebastian";
 //console.log(nama);
 
-for (var i=0; i<10; i++){
-    console.log(i);
+
+/* 
+    Scope dalam Javascript
+    1. Function Scope (Var)
+    for (var i=0; i<10; i++){
+        console.log(i);
+    }
+    console.log(i); // variable i di akses di luar for
+
+
+    2. Block Scope
+    // Block Scope -> Variabel i tidak dapat di akses dari luar
+    function tes(){
+        for (var i=0; i<10; i++){
+            console.log(i);
+        }
+    }
+    tes();
+*/
+
+/*
+    IIFE
+    (function tes(){
+        for (var i=0; i<10; i++){
+            console.log(i);
+        }
+    })();
+
+// Make it Block Scope
+{
+    let i;
+    for (let i=0;i<10;i++){
+        console.log(i);
+    }
 }
 console.log(i);
+*/
+const person = {
+    firstName: "Sebastian",
+    lastName: "Ganna",
+    age: 33,
+};
+console.log(
+    "Hallo nama saya "+ person.firstName 
+    + " " + person.lastName 
+    + ". Umur saya adalah " + person.age 
+    + " tahun."
+);
+
+console.log(
+    `Hallo nama saya ${person.firstName} ${person.lastName}. Umur saya ${person.age} tahun`
+);
