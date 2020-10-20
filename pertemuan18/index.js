2b6a8a95dacfc071c7d6d78aa2d2d91206925e73
gannasebastian <s21710035@student.ac.id>
2020-10-20 16:37:56 +0800

    18th Commit

--- pertemuan18/index.js
+++ pertemuan18/index.js
@@ -1,3 +1,47 @@
 /*
     Asynchronous JavaScript
+    20 Oktober 2020
 */
+
+// const second = () => {
+//     setTimeout(() => {
+//         console.log('Second');
+//     }, 2000);
+// }
+
+// Make 3 async (Jeda 2 Detik)
+// const second = () => {
+//     setTimeout(() => {
+//         console.log('Async 1');
+//         setTimeout(() => {
+//             console.log('Async 2');
+//             setTimeout(() => {
+//                 console.log('Async 3');
+//             }, 2000);
+//         }, 2000);
+//     }, 2000);
+// }
+
+// const first = () => {
+//     console.log('First');
+//     second();
+//     console.log('The End');
+// }
+
+// first(); // Run Function
+
+// PROMISE
+const getData = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        const error = true;
+        if(error){
+            reject();
+        }
+        else{
+            resolve();
+        }
+    }, 2000);
+})
+getData
+    .then(() => console.log('Success'))
+    .catch(() => console.log('Error'))
